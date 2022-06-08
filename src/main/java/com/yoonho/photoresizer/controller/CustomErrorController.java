d50623e0f1ecf20ba41c8baa84c83f440c59be19
yoonho <cyh7834@gmail.com>
2022-06-08 22:20:13 +0900

    [ADD] 커스텀 에러 페이지 테스트 진행

---
+++ src/main/java/com/yoonho/photoresizer/controller/CustomErrorController.java
@@ -0,0 +1,29 @@
+package com.yoonho.photoresizer.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null){
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error/400error";
+            } else {
+                return "error/error";
+            }
+        }
+
+        return "error/error";
+    }
+}
